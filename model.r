fe546eb565cef6183b4793fded5194c892ca7706
cqian <cqian@ucalgary.ca>
2015-12-05 22:50:40 -0500

    model

--- model.r
+++ model.r
@@ -8,33 +8,40 @@ options(mc.cores = parallel::detectCores())
 data <- read_rdump('output/seq.r')
 fit <- stan(file="model.stan", data = data, iter = 50, chains = 1)
 
-# fit1 <- stan(fit=fit, data = CTMdata, iter = 50, chains = 1, algorithm = "NUTS")
+# fit0 = stan(fit=fit0, data = data, iter = 50, chains = 1, algorithm = "NUTS")
 # fit1 <- stan(fit=fit, data = CTMdata, iter = 1000, chains = 4)
 
-print(fit)
-plot(fit)
+# extract result
+theta <- extract(fit, 'theta', permuted = TRUE)
+post_mean_theta <- get_posterior_mean(fit,'theta')
 
+# get the log-posterior for all chains
+log_post_model <- get_logposterior(fit)
+lp <- extract(fit, 'lp__', permuted = TRUE)
 
-la <- extract(fit, permuted = TRUE) 
-a <- extract(fit, permuted = FALSE) 
-
-plot(fit, pars = c("theta"))
-theta <- extract(fit, 'theta')
-theta <- unlist(theta, use.names=FALSE)
-plot(density(theta),
-     xlab=expression(theta), col=grey(0, 0.8),
-     main="Parameter distribution")
-
-plot(fit, pars=c("phi")) 
-phi <- extract(fit, 'phi')
-phi <- unlist(phi, use.names=FALSE)
-plot(density(phi),
-     xlab=expression(phi), col=grey(0, 0.8),
-     main="Parameter distribution")
-
+# # print result
+# print(fit)
+# plot(fit) 
+# 
+# # plot result
+# plot(fit, pars = c("theta"))
+# theta <- extract(fit, 'theta')
+# theta <- unlist(theta, use.names=FALSE)
+# plot(density(theta),
+#      xlab=expression(theta), col=grey(0, 0.8),
+#      main="Parameter distribution")
+# 
+# plot(fit, pars=c("phi")) 
+# phi <- extract(fit, 'phi')
+# phi <- unlist(phi, use.names=FALSE)
+# plot(density(phi),
+#      xlab=expression(phi), col=grey(0, 0.8),
+#      main="Parameter distribution")
+#
 # plot(fit, pars = c("eta"))
 # eta <- extract(fit, 'eta')
 # eta <- unlist(eta, use.names=FALSE)
 # plot(density(eta),
 #      xlab=expression(eta), col=grey(0, 0.8),
 #      main="Parameter distribution")
+
